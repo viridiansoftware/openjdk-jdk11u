d1bb08275e730e120f0bb5ceb6d0d7c4673a54d8
Thomas Cashman <thomascashman404@gmail.com>
2019-04-12 23:46:11 +0100

    Added gensrc

---
+++ build/linux-x86_64-normal-server-release/support/gensrc/jdk.jshell/jdk/internal/jshell/tool/resources/version.java
@@ -0,0 +1,11 @@
+package jdk.internal.jshell.tool.resources;
+
+public final class version extends java.util.ListResourceBundle {
+    protected final Object[][] getContents() {
+        return new Object[][] {
+            { "full", "11.0.3-internal+0-adhoc.thomas.openjdk-jdk11u" },
+            { "jdk", "11.0.3" },
+            { "release", "11.0.3-internal" },
+        };
+    }
+}
